d0c265583d57221f0f73c6f55a7e1861686d24b0
Rimeshsapkota <rimeshsapkota12345@gmail.com>
2026-03-18 12:11:48 +0545

    PDM-4 remove unwanted code

--- smart-hospital-auth-service/src/main/java/com/smarthospital/authservice/JwtUtil.java
+++ smart-hospital-auth-service/src/main/java/com/smarthospital/authservice/JwtUtil.java
@@ -1,38 +1,38 @@
-package com.smarthospital.authservice;
-
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.context.annotation.Configuration;
-
-import java.security.Key;
-import java.util.Date;
-
-@Configuration
-public class JwtUtil {
-
-    private static final String SECRET =
-            "mySuperSecretKeyForJwtGeneration123456";
-
-    private static final Key KEY =
-            Keys.hmacShaKeyFor(SECRET.getBytes());
-
-    public  String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(
-                        System.currentTimeMillis() + 86400000))
-                .signWith(KEY)
-                .compact();
-    }
-    public static String extractUsername(String token) {
-
-        return Jwts.parserBuilder()
-                .setSigningKey(KEY)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-}
+//package com.smarthospital.authservice;
+//
+//
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.security.Key;
+//import java.util.Date;
+//
+//@Configuration
+//public class JwtUtil {
+//
+//    private static final String SECRET =
+//            "mySuperSecretKeyForJwtGeneration123456";
+//
+//    private static final Key KEY =
+//            Keys.hmacShaKeyFor(SECRET.getBytes());
+//
+//    public  String generateToken(String username) {
+//        return Jwts.builder()
+//                .setSubject(username)
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(
+//                        System.currentTimeMillis() + 86400000))
+//                .signWith(KEY)
+//                .compact();
+//    }
+//    public static String extractUsername(String token) {
+//
+//        return Jwts.parserBuilder()
+//                .setSigningKey(KEY)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//}
