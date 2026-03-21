75efa3f0a7d19544d59f98919f98c3b850436dc3
Rimeshsapkota <rimeshsapkota12345@gmail.com>
2026-03-21 22:56:53 +0545

    update controller class

--- smart-hospital-auth-service/src/main/java/com/smarthospital/authservice/admin/HospitalRegisterController.java
+++ smart-hospital-auth-service/src/main/java/com/smarthospital/authservice/admin/HospitalRegisterController.java
@@ -7,16 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class HospitalRegisterController {
     private final HospitalRegisterService hospitalService;
@@ -49,6 +46,5 @@ public class HospitalRegisterController {
                 .subscribeOn(Schedulers.boundedElastic())
                 .map(ResponseEntity::ok);
     }
-
 
 }
