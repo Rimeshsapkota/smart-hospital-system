fb6fbc1bf670d6d011783ee7874c229299bc0780
Rimeshsapkota <rimeshsapkota12345@gmail.com>
2026-04-18 08:29:21 +0545

    update in code

--- smart-hospital-common-lib/src/main/java/com/smarthospital/common_lib/pagination/BaseService.java
+++ smart-hospital-common-lib/src/main/java/com/smarthospital/common_lib/pagination/BaseService.java
@@ -1,7 +1,6 @@
 package com.smarthospital.common_lib.pagination;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
